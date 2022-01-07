8d2f14110f4b1b32558c73f3d7213400e7b43800
Erick Ribeiro <erickrocha965@gmail.com>
2022-01-07 11:08:35 -0300

    pages-project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contato from './components/pages/Contact';
+import Sobre  from './components/pages/Sobre';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
+
+
 function App() {
   return (
-    <p>Cost</p>
+    <div>
+      <Router>
+        <Navbar />
+        <Container customClass= "min_heigth">
+          <Routes>
+            <Route path="/" element={<Home />} ></Route>
+            <Route path="/contact" element={<Contato />} ></Route>
+            <Route path="/projects" element={<Projects />} ></Route>
+            <Route path="/about" element={<Sobre />} ></Route>
+            <Route path="/newproject" element={<NewProject />} ></Route>
+          </Routes>
+        </Container>
+      </Router>
+      <Footer />
+    </div>
   )
 }
 
