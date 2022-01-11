d80bbf23e6c8290a04024776bf3bae820b13ba61
Erick Ribeiro <erickrocha965@gmail.com>
2022-01-11 15:31:34 -0300

    cost-projects

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,43 @@
-function NewProject(){
-    return <h1>Novo Projeto</h1>
+import { useNavigate } from 'react-router'
+
+import styles from './NewProject.module.css'
+import ProjectForm from './project/ProjectForm'
+
+
+function NewProject() {
+
+    const history = useNavigate()
+
+    function createPost(project) {
+        //initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }, 
+            body: JSON.stringify(project)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data);
+            //redirect
+            history('/projects', {state:{
+                message: 'Projeto criado com sucesso!'
+            }
+            })
+        })
+        .catch((err) => console.log(err))
+    }
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
+        </div>
+    )
 }
 
 export default NewProject
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,63 @@
-function Project  (){
-    return <h1>Project </h1>
+import { useLocation } from "react-router"
+
+import Message from "../layout/Message"
+import Container from "../layout/Container"
+import Loading from "../layout/Loading"
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "./project/ProjectCard"
+
+import styles from "./Projects.module.css"
+import { useEffect, useState } from "react"
+
+function Project() {
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }).then((resp) => resp.json())
+                .then((data) => {
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 500)
+    }, [])
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus project</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
+            {message && <Message type="sucess" msg={message} />}
+            <Container customClass="start">
+                {projects.length > 0 &&
+                    projects.map((project) => ( <ProjectCard
+                        name={project.name}
+                        id={project.id}
+                        category={project.category.name}
+                        budget={project.budget}
+                        key={project.id}
+                    />))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados</p>
+                )} 
+            </Container>
+        </div>
+    )
 }
 
 export default Project 
